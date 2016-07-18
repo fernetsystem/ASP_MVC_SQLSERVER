49adf3d0ee3b16efaf8b1075ac7b0e4a020a30a6
Jth Rdz <fernetsystem@gmail.com>
2016-07-18 03:57:06 -0500

    controlador de clientes para llamar metodos

---
+++ testPrb01/Controller/clientesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using testPrb01.Models;
+
+namespace testPrb01.Controller
+{
+    public class clientesController
+    {
+        ClientesImpl clientesImpl = new ClientesImpl();
+        public int saveClient(Clientes cl)
+        {
+            return clientesImpl.insertClient(cl);
+        }
+
+        public IEnumerable<Clientes> getAllClients()
+        {
+            return clientesImpl.listClient();
+        }
+    }
+}
