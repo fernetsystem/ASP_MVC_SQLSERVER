bc408c3619cd658f49cbe41fa9cf94897e3774ce
Jth Rdz <fernetsystem@gmail.com>
2016-07-18 03:55:09 -0500

    modelo cliente

---
+++ testPrb01/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testPrb01.Models
+{
+    public class Clientes
+    {
+        public int idCliente { get; set; }
+        public string nombre { get; set; }
+        public string paterno { get; set; }
+        public string materno { get; set; }
+        public string sexo { get; set; }
+        public string estado { get; set; }
+
+    }
+}
